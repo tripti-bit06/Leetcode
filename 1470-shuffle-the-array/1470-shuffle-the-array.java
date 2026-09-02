664d38823754b335a3b7e857aecb29662f641e6e
Tripti <tripti.g2006@gmail.com>
2026-09-02 09:22:23 +0530

    Time: 0 ms (100%), Space: 46.5 MB (41.34%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];       // Places x_i at even indices (0, 2, 4...)
+            result[2 * i + 1] = nums[i + n]; // Places y_i at odd indices (1, 3, 5...)
+        }
+        return result;
+    }
+}
